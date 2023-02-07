f98538bcdde3776cfd9dc7b5ccfe00305368eaad
Matheus <matheuscroesy@gmail.com>
2023-02-06 21:15:01 -0300

    Importação de Icons do FontAwesome

---
+++ src/FontsAwesome.js
@@ -0,0 +1,13 @@
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const icons = {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+};
+
+export default icons;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import icons from "./FontsAwesome";
 import App from "./App.vue";
 import router from "./router";
 
+/* add the imported icons to the library */
+library.add({ ...icons });
 const app = createApp(App);
+/* register font awesome icon component */
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(router);
-
 app.mount("#app");
